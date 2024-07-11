05d37a18fe3b2b791630322e0c571af9e6dd7c3a
Siyang He <s.he@vehikl.com>
2024-07-11 17:15:22 -0400

    conditionally disable collapse

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import {CheckInSuccessDialog} from "./components/dialogs/check-in-success-dialog
 import {CheckInFailureDialog} from "./components/dialogs/check-in-failure-dialog.tsx";
 import {CheckOutSuccessDialog} from "./components/dialogs/check-out-success-dialog.tsx";
 import {CheckOutFailureDialog} from "./components/dialogs/check-out-failure-dialog.tsx";
+import {Collapse} from "antd";
 
 type CheckinResult = {
   success: true;
@@ -47,83 +48,117 @@ function App() {
 
   const freeParkingSpotCount = garage.totalSpots - garage.occupants.length;
 
+  const checkinForm = <ParkingForm
+    timestampLabel='Entry Time'
+    passedBusinessLogicValidation={checkinResult.success !== false}
+    onSubmit={(payload) => {
+      try {
+        const newSpot = checkin(payload);
+        setCheckinResult({
+          success: true,
+          spot: newSpot,
+        });
+      } catch (e) {
+        setCheckinResult({
+          success: false,
+          errorMsg: (e as Error).message,
+        });
+      } finally {
+        setShouldShowCheckinModal(true);
+      }
+    }}
+  />;
+
+  const checkoutForm = <ParkingForm
+    timestampLabel='Exit Time'
+    passedBusinessLogicValidation={checkoutResult.success !== false}
+    onSubmit={(payload) => {
+      try {
+        const checkedOutCar = checkout(payload);
+        setCheckoutResult({
+          success: true,
+          car: checkedOutCar,
+        });
+      } catch (e) {
+        setCheckoutResult({
+          success: false,
+          errorMsg: (e as Error).message,
+        });
+      } finally {
+        setShouldShowCheckoutModal(true);
+      }
+    }}
+  />;
+
   return (
     <>
       <div>{freeParkingSpotCount} parking spot{(freeParkingSpotCount !== 1) && 's'} left</div>
-      <button>Check in car</button>
-      <ParkingForm
-        timestampLabel='Entry Time'
-        passedBusinessLogicValidation={checkinResult.success !== false}
-        onSubmit={(payload) => {
-          try {
-            const newSpot = checkin(payload);
-            setCheckinResult({
-              success: true,
-              spot: newSpot,
-            });
-          } catch (e) {
-            setCheckinResult({
-              success: false,
-              errorMsg: (e as Error).message,
-            });
-          } finally {
-            setShouldShowCheckinModal(true);
-          }
-        }}
+      <Collapse
+        {...freeParkingSpotCount ? {} : {collapsible: "disabled"}}
+        items={[
+          {
+            key: 'checkinForm',
+            label: 'Check in car',
+            children: (
+              <>
+                {checkinForm}
+                {checkinResult.success === true &&
+                    <CheckInSuccessDialog
+                        open={shouldShowCheckinModal}
+                        closeModal={closeCheckinModal}
+                        spot={checkinResult.spot}
+                    />}
+                {checkinResult.success === false &&
+                    <CheckInFailureDialog
+                        open={shouldShowCheckinModal}
+                        closeModal={closeCheckinModal}
+                        errMsg={checkinResult.errorMsg}
+                    />}
+              </>
+            )
+          },
+        ]}
+        />
+      <Collapse
+        {...freeParkingSpotCount < garage.totalSpots ? {} : {collapsible: "disabled"}}
+        items={[
+          {
+            key: 'checkoutForm',
+            label: 'Check out car',
+            children: (
+              <>
+                {checkoutForm}
+                {checkoutResult.success === true &&
+                    <CheckOutSuccessDialog
+                        open={shouldShowCheckoutModal}
+                        closeModal={closeCheckoutModal}
+                        car={checkoutResult.car}
+                    />}
+                {checkoutResult.success === false &&
+                    <CheckOutFailureDialog
+                        open={shouldShowCheckoutModal}
+                        closeModal={closeCheckoutModal}
+                        errMsg={checkoutResult.errorMsg}
+                    />}
+              </>
+            )
+          },
+        ]}
       />
-      {checkinResult.success === true &&
-        <CheckInSuccessDialog
-            open={shouldShowCheckinModal}
-            closeModal={closeCheckinModal}
-            spot={checkinResult.spot}
-        />}
-      {checkinResult.success === false &&
-        <CheckInFailureDialog
-            open={shouldShowCheckinModal}
-            closeModal={closeCheckinModal}
-            errMsg={checkinResult.errorMsg}
-        />}
-      <button>Check out car</button>
-      <ParkingForm
-        timestampLabel='Exit Time'
-        passedBusinessLogicValidation={checkoutResult.success !== false}
-        onSubmit={(payload) => {
-          try {
-            const checkedOutCar = checkout(payload);
-            setCheckoutResult({
-              success: true,
-              car: checkedOutCar,
-            });
-          } catch (e) {
-            setCheckoutResult({
-              success: false,
-              errorMsg: (e as Error).message,
-            });
-          } finally {
-            setShouldShowCheckoutModal(true);
-          }
-        }}
+      <Collapse
+        {...garage.occupants.length ? {} : {collapsible: "disabled"}}
+        items={[
+          {
+            key: 'details',
+            label: 'Details',
+            children: garage.occupants.map((parkingSpot) =>
+                  <ParkingSpotDetailCard
+                    key={parkingSpot.licensePlate}
+                    parkingSpot={parkingSpot}
+                  />),
+          },
+        ]}
       />
-      {checkoutResult.success === true &&
-        <CheckOutSuccessDialog
-            open={shouldShowCheckoutModal}
-            closeModal={closeCheckoutModal}
-            car={checkoutResult.car}
-        />}
-      {checkoutResult.success === false &&
-        <CheckOutFailureDialog
-            open={shouldShowCheckoutModal}
-            closeModal={closeCheckoutModal}
-            errMsg={checkoutResult.errorMsg}
-        />}
-      <button>Details</button>
-      {garage.occupants.length
-      ? garage.occupants.map((parkingSpot) =>
-          <ParkingSpotDetailCard
-            key={parkingSpot.licensePlate}
-            parkingSpot={parkingSpot}
-          />)
-      : 'No parked cars.'}
     </>
   )
 }
